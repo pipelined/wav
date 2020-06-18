a7d3982857a74d926788598e23711965c46a78c8
Sergei Dudka <serg.dudk@gmail.com>
2020-06-18 21:47:37 +0200

    adopt new pipe and signal packages

--- wav_test.go
+++ wav_test.go
@@ -1,28 +1,22 @@
 package wav_test
 
 import (
-	"math"
+	"context"
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
+	"pipelined.dev/pipe"
 	"pipelined.dev/signal"
 	"pipelined.dev/wav"
 )
 
 const (
 	bufferSize = 512
-	wavSamples = 330534
 	wavSample  = "_testdata/sample.wav"
 	wav1       = "_testdata/out1.wav"
 	wav2       = "_testdata/out2.wav"
 	wav3       = "_testdata/out3.wav"
 	notWav     = "wav.go"
-)
-
-var (
-	wavMessages = int(math.Ceil(float64(wavSamples) / float64(bufferSize)))
 )
 
 func TestWavPipe(t *testing.T) {
@@ -49,54 +43,23 @@ func TestWavPipe(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		inFile, err := os.Open(test.inPath)
-		assert.Nil(t, err)
+		inFile, _ := os.Open(test.inPath)
 		pump := wav.Pump{ReadSeeker: inFile}
 
-		outFile, err := os.Create(test.outPath)
-		assert.Nil(t, err)
+		outFile, _ := os.Create(test.outPath)
 		sink := wav.Sink{
 			WriteSeeker: outFile,
 			BitDepth:    test.bitDepth,
 		}
+		line, err := pipe.Routing{
+			Source: pump.Pump(),
+			Sink:   sink.Sink(),
+		}.Line(bufferSize)
 
-		pumpFn, sampleRate, numChannles, err := pump.Pump("")
-		assert.NotNil(t, pumpFn)
-		assert.Nil(t, err)
-
-		sinkFn, err := sink.Sink("", sampleRate, numChannles)
-		assert.NotNil(t, sinkFn)
-		assert.Nil(t, err)
-
-		buf := signal.Float64Buffer(numChannles, bufferSize)
-		messages, samples := 0, 0
-		for {
-			if err = pumpFn(buf); err != nil {
-				break
-			}
-			_ = sinkFn(buf)
-			messages++
-			if buf != nil {
-				samples += len(buf[0])
-			}
+		p := pipe.New(context.Background(), pipe.WithLines(line))
+		err = p.Wait()
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
 		}
-
-		assert.Equal(t, wavMessages, messages)
-		assert.Equal(t, wavSamples, samples)
-
-		err = sink.Flush("")
-		assert.Nil(t, err)
-
-		err = inFile.Close()
-		assert.Nil(t, err)
-		err = outFile.Close()
-		assert.Nil(t, err)
 	}
-}
-
-func TestWavPumpErrors(t *testing.T) {
-	f, _ := os.Open(notWav)
-	pump := wav.Pump{ReadSeeker: f}
-	_, _, _, err := pump.Pump("")
-	assert.NotNil(t, err)
 }
